52afd1312938ec12c96a159ae01cb132164887ef
axxe.eth <maxime.brugel@gmail.com>
2022-09-21 17:44:25 +0200

    test: todo list

--- test/SuperTank.t.sol
+++ test/SuperTank.t.sol
@@ -7,7 +7,25 @@ import {SuperTank} from "src/SuperTank.sol";
 contract TestContract is Test {
     function setUp() public {}
 
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
-    }
+    // TODO => Cannot deposit gobbler if not the owner
+
+    // TODO => Deposit first Gobbler but NO Goo already deposited
+
+    // TODO => Deposit first Gobbler with some Goo already deposited
+
+    // TODO => Deposit second Gobbler with 0 Goo already deposited and gooAmount = 0
+
+    // TODO => Deposit second Gobbler with 0 Goo already deposited and gooAmount != 0
+
+    // TODO => Cannot deposit Gobbler with wrong Goo Amount
+
+    // TODO => Cannot withdraw Gobbler if not the initial depositor
+
+    // TODO => Deposit goo but no gobblers deposited
+
+    // TODO => Deposit goo with one Gobbler deposited
+
+    // TODO => Withdraw goo but no gobblers deposited
+
+    // TODO => Withdraw goo with one Gobbler deposited
 }
